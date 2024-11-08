cbbb24e4e9e583a8cc91c48ee3e6b4e4f435e59b
Jmik Thang <jmikthang5@gmail.com>
2024-11-08 17:54:37 -0500

    add itemModal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,87 @@
-import {useState} from "react"; 
-import './App.css'
-import Header from '../Header/Header'
-import Main from '../Main/Main'
+import { useState } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/itemModal";
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
+  const [activeModal, setActiveModal] = useState("preview");
+  const [selectedCard, setSelectedCard] = useState({});
 
-  const [weatherData, setWeatherData] = useState({type: "hot"})
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
 
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
   return (
     <div className="page">
       <div className="page__content">
-        <Header/>
-        <Main weatherData={weatherData}/>
+        <Header handleAddClick={handleAddClick} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        <ModalWithForm
+          title="New garment"
+          buttonText="Add garment"
+          activeModal={activeModal}
+          handleCloseClick={closeActiveModal}
+        >
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              type="text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="imageUrl" className="modal__label">
+            Image{" "}
+            <input
+              type="text"
+              className="modal__input"
+              id="imageUrl"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-button">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal___label_type_radio"
+            >
+              <input type="radio" className="modal__radio-input" id="hot" />
+              Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal___label_type_radio"
+            >
+              <input type="radio" className="modal__radio-input" id="warm" />
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal___label_type_radio"
+            >
+              <input type="radio" className="modal__radio-input" id="cold" />
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
+        <ItemModal
+          activeModal={activeModal}
+          card={selectedCard}
+          onClick={handleCardClick}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import "./Header.css"
 import logo from "../../images/logo.svg"
 import avatar from "../../images/avatar.png"
-function Header() { 
+function Header({handleAddClick}) { 
       return(
             <header className="header">
                   <img src={logo} alt="header logo" className="header__logo" />
                   <p className="header__date-and-location">Date, Location</p>
-                  <button className="header__add-clothes-btn">+ Add clothes</button>
+                  <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+ Add clothes</button>
                   <div className="header__user-container">
                         <p className="header__username">Terrence Tegegne</p>
                         <img src={avatar} alt="avatar" className="header__avatar" />
